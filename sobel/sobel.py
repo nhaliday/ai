b72b364501aff600fa52ec8a5681a8daaccc152d
Nick Haliday <nick.haliday@gmail.com>
2012-01-26 17:33:27 -0500

    numpy

--- sobel/sobel.py
+++ sobel/sobel.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # Nick Haliday
 # Lab 11: Sobel Edge Detection
 # 2012-01-24
@@ -9,18 +9,15 @@ import os
 from os import path
 import argparse
 
-from math import *
+import numpy as np
+
 from itertools import *
 
 
-def grouper(n, iterable, fillvalue=None):
-    "grouper(3, 'ABCDEFG', 'x') --> ABC DEF Gxx"
-    args = [iter(iterable)] * n
-    return zip_longest(fillvalue=fillvalue, *args)
-
-
-def grayscale(r, g, b):
-    return int(round(0.30*r + 0.59*g + 0.11*b))
+def grayscale(rgb, g=None, b=None):
+    if g is not None:
+        rgb = [rgb, g, b]
+    return int(round(0.30*rgb[0] + 0.59*rgb[1] + 0.11*rgb[2]))
 
 
 def main():
@@ -30,25 +27,20 @@ def main():
     args = parser.parse_args()
     
     name, ext = os.path.splitext(args.image)
-    assert(ext == '.ppm')
     with open(args.image) as fin, open(name + '.pgm', 'w') as fout:
         data = fin.read().split()
-        assert(data[0] == 'P3')
 
         M, N = map(int, data[1:3])
         scale = int(data[3])
 
-        pixels = map(int, data[4:])
+        pixels = np.array(data[4:], dtype='int')
+        rgb = pixels.reshape(N, M, 3)
+        gray = np.apply_along_axis(grayscale, 2, rgb)
 
-        rgb = grouper(3, pixels)
-        gray = starmap(grayscale, rgb)
-        lines = grouper(M, map(str, gray))
-
-        print('P2', file=fout)
-        print(M, N, file=fout)
-        print(scale, file=fout)
-        for line in lines:
-            print(' '.join(line), file=fout)
+        print >>fout, 'P2'
+        print >>fout, M, N
+        print >>fout, scale
+        print >>fout, ' '.join(map(str, gray.flat))    
 
 
 if __name__ == "__main__":
