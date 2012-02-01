8754c7de7fac56210e6a1d2fc4141a36bb3c476b
Nick Haliday <nick.haliday@gmail.com>
2012-02-01 08:42:57 -0500

    numpy

--- edge/sobel.py
+++ edge/sobel.py
@@ -9,15 +9,15 @@ import os
 from os import path
 import argparse
 
-import numpy as np
-
-from itertools import *
+import scipy as sp
 
 
-def grayscale(rgb, g=None, b=None):
-    if g is not None:
-        rgb = [rgb, g, b]
-    return int(round(0.30*rgb[0] + 0.59*rgb[1] + 0.11*rgb[2]))
+GRAYSCALE = sp.array([.30, .59, .11])
+
+
+def grayscale(arr):
+    return (arr[:, :, 0] * GRAYSCALE[0] + arr[:, :, 1] * GRAYSCALE[1] +
+            arr[:, :, 2] * GRAYSCALE[2]).astype(sp.int32)
 
 
 def main():
@@ -30,17 +30,17 @@ def main():
     with open(args.image) as fin, open(name + '.pgm', 'w') as fout:
         data = fin.read().split()
 
-        M, N = map(int, data[1:3])
-        scale = int(data[3])
+        M, N, scale = map(int, data[1:4])
 
-        pixels = np.array(data[4:], dtype='int')
+        pixels = sp.array(data[4:], dtype=sp.int32)
         rgb = pixels.reshape(N, M, 3)
-        gray = np.apply_along_axis(grayscale, 2, rgb)
+        gray = grayscale(rgb)
+        sgray = gray.astype('|S{}'.format(len(str(scale))))
 
         print >>fout, 'P2'
         print >>fout, M, N
         print >>fout, scale
-        print >>fout, ' '.join(map(str, gray.flat))    
+        print >>fout, ' '.join(sgray.flat)    
 
 
 if __name__ == "__main__":
