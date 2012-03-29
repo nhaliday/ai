bec11b8694490cac7ed68f95fe52e7be7fdf7d4f
Nick Haliday <nick.haliday@gmail.com>
2012-03-29 12:04:52 -0400

    segregation modification

--- segregation/segregation.py
+++ segregation/segregation.py
@@ -8,8 +8,8 @@ import random
 
 
 def neighbors(r, c, rows, cols):
-    for dr in range(-1, 2):
-        for dc in range(-1, 2):
+    for dr in range(-1, 1 + 1):
+        for dc in range(-1, 1 + 1):
             r_ = r + dr
             c_ = c + dc
             if 0 <= r_ < rows and 0 <= c_ < cols:
@@ -27,7 +27,7 @@ class Population:
         self.rows = rows
         self.cols = cols
 `       self.matrix = [[BLANK] * cols] * rows
-        self.satisfied = False
+        self.allsatisfied = False
 
         # populate the matrix
         numadd = int(DENSITY * rows * cols)
@@ -40,11 +40,31 @@ class Population:
                 self.matrix[r][c] = random.choice((X, O))
                 numadd -= 1
 
+    def satisfied(self, r, c):
+        if (0 <= r < self.rows and 0 <= c < self.cols and
+                self.matrix[r][c] != Population.BLANK):
+            n, m = 0, 0
+            for r_, c_ in neighbors(r, c, self.rows, self.cols):
+                if self.matrix[r_][c_] != BLANK:
+                    n += 1
+                if self.matrix[r_][c_] == self.matrix[r][c]:
+                    m += 1
+            if 1 <= n <= 3:
+                return m >= 1
+            elif 3 <= n < 6:
+                return m >= 2
+            elif 6 <= n:
+                return m >= 3
+        return True
+
+    def displace(self, r, c):
+        pass
+
     def shuffle():
-        self.satisfied = True
+        self.allsatisfied = True
         for r in range(self.rows):
             for c in range(self.cols):
                 if not self.satisfied(r, c):
-                    self.satisfied = False
-                    self.jiggle(r, c)
+                    self.allsatisfied = False
+                    self.displace(r, c)
 
